1236dccbd532e542e124305d932423de0e3d86a3
Kalebe Santos <kalebejogos.kk@gmail.com>
2023-10-01 21:58:30 -0300

    x

--- src/js/index.js
+++ src/js/index.js
@@ -14,3 +14,26 @@ a seleção dele
         passo 3 - verificar se já exista um personagem selecionado, se sim, devemos remover 
 a seleção dele 
 */
+
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+	botao.addEventListener("click", () => {
+		desselecionarBotao();
+		desselecionarPersonagem();
+
+		botao.classList.add("selecionado");
+		personagens[indice].classList.add("selecionado");
+	});
+});
+
+function desselecionarPersonagem() {
+	const personagemSelecionado = document.querySelector(".personagem.selecionado");
+	personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+	const botaoSelecionado = document.querySelector(".botao.selecionado");
+	botaoSelecionado.classList.remove("selecionado");
+}
